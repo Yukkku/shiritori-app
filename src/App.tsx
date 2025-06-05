b0e80780be863dcd7586c822132f9742e4cde076
Yukkku <yukku.scratcher@gmail.com>
2025-06-05 17:18:37 +0900

    簡易的なしりとりを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
+import { createSignal } from 'solid-js';
+
 export default () => {
-  return <></>;
+  const [lastWord, setlastWord] = createSignal('しりとり');
+  let wordInput: HTMLInputElement;
+  return (<>
+    <h1>しりとり</h1>
+    <div>Last Word: {lastWord()}</div>
+    <input ref={elem => wordInput = elem} onKeyDown={(e) => {
+      if (e.key !== "Enter") return;
+      const word = wordInput.value;
+      if (word === '') return;
+      if (word[0] !== lastWord().at(-1)) {
+        alert('つながってないよ!');
+        return;
+      }
+      setlastWord(word);
+      wordInput.value = '';
+    }} />
+  </>);
 };
