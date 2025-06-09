2a3f3ee7cb5b546b55014332fa26b751d32d509b
Yukkku <yukku.scratcher@gmail.com>
2025-06-09 15:10:59 +0900

    色の環境差異が少なくなるように設定

--- src/Error.tsx
+++ src/Error.tsx
@@ -12,7 +12,7 @@ export default (prop: {
         M6,0A6,6 0 0,1 6,12A6,6 0 0,1 6,0Z
         M7,2 5,2 5,7 7,7Z
         M7,8 5,8 5,10 7,10Z
-      " fill="color-mix(in srgb, red, canvasText)" />
+      " fill="var(--error-icon)" />
     </svg>
     {prop.msg}
   </div>);
