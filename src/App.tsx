ff7756a4e982ff006d76e00635c04e0691ab968e
Yukkku <yukku.scratcher@gmail.com>
2025-06-09 14:44:31 +0900

    開発しやすいよう, 書き方を変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,42 @@
-import { createSignal, Show } from "solid-js";
+import { createSignal, For, Show } from "solid-js";
 import styles from "./App.module.css";
 import Error from "./Error.tsx";
-import LastWord from "./LastWord.tsx";
 
 export default () => {
-  const [lastWord, setlastWord] = createSignal("しりとり");
+  const [history, setHistory] = createSignal<string[]>(["しりとり"]);
   const [errFlg, setErrFlg] = createSignal(false);
-  const used = new Set<string>();
   const reset = () => {
-    setlastWord("しりとり");
-    used.clear();
+    setHistory(["しりとり"]);
+    wordInput.value = '';
   };
   let wordInput: HTMLInputElement;
+  let scrollwrapper: HTMLDivElement;
   return (<div class={styles.app}>
     <h1>しりとり</h1>
-    <LastWord word={lastWord()} />
-    <input ref={elem => wordInput = elem} class={styles.wordinput} onKeyDown={(e) => {
-      if (e.key !== "Enter") return;
-      const word = wordInput.value;
-      if (word === '') return;
-      if (word[0] !== lastWord().at(-1)) {
-        setErrFlg(true);
-        return;
-      }
-      setErrFlg(false);
-      if (word.at(-1) === "ん") {
-        alert("「ん」で終わってる!");
-        reset();
-      } else if (used.has(word)) {
-        alert("その単語は2回目だよ!");
-        reset();
-      } else {
-        setlastWord(word);
-        used.add(word);
-        wordInput.value = "";
-      }
-    }} />
+    <div ref={elem => scrollwrapper = elem} class={styles.scrollwrapper}>
+      <For each={history()}>{word => <input value={word} disabled />}</For>
+      <input ref={elem => wordInput = elem} onKeyDown={(e) => {
+        if (e.key !== "Enter") return;
+        const word = wordInput.value;
+        if (word === '') return;
+        if (word[0] !== history().at(-1)!.at(-1)) {
+          setErrFlg(true);
+          return;
+        }
+        setErrFlg(false);
+        if (word.at(-1) === "ん") {
+          alert("「ん」で終わってる!");
+          reset();
+        } else if (history().includes(word)) {
+          alert("その単語は2回目だよ!");
+          reset();
+        } else {
+          setHistory(history => [...history, word]);
+          wordInput.scrollIntoView({ behavior: "smooth" });
+          wordInput.value = "";
+        }
+      }} />
+    </div>
     <Show when={errFlg()}><Error msg="前の単語とつながってないよ!" /></Show>
     <button class={styles.reset} onClick={reset}>リセット</button>
   </div>);
