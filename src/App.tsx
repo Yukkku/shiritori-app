858e05687eb80cdfb958b2b389c8fa126e2da163
Yukkku <yukku.scratcher@gmail.com>
2025-06-10 13:05:27 +0900

    終了画面を作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { createSignal, For, Show } from "solid-js";
 import styles from "./App.module.css";
 import Error from "./Error.tsx";
+import Result from "./Result.tsx";
 
 export default () => {
   const [history, setHistory] = createSignal<string[]>(["しりとり"]);
   const [errFlg, setErrFlg] = createSignal(false);
+  const [finMsg, setFinMsg] = createSignal<string | null>(null);
   const reset = () => {
     setHistory(["しりとり"]);
     wordInput.value = '';
     setErrFlg(false);
+    setFinMsg(null);
   };
   let wordInput: HTMLInputElement;
   return (<div class={styles.app}>
@@ -25,19 +28,18 @@ export default () => {
         }
         setErrFlg(false);
         if (word.at(-1) === "ん") {
-          alert("「ん」で終わってる!");
-          reset();
+          setFinMsg("「ん」で終わってしまった!");
         } else if (history().includes(word)) {
-          alert("その単語は2回目だよ!");
-          reset();
+          setFinMsg(`「${word}」を2回使ってしまった!`);
         } else {
           setHistory(history => [...history, word]);
           wordInput.scrollIntoView({ behavior: "smooth" });
           wordInput.value = "";
         }
-      }} />
+      }} readonly={finMsg() != null} />
     </div>
     <Show when={errFlg()}><Error msg="前の単語とつながってないよ!" /></Show>
+    <Show when={finMsg() != null}><Result reason={finMsg()!} history={history()} onRetry={reset} /></Show>
     <button class={styles.reset} onClick={reset}>リセット</button>
   </div>);
 };
---
+++ src/Result.tsx
@@ -0,0 +1,12 @@
+import styles from "./Result.module.css";
+
+export default (prop: { reason: string, history: string[], onRetry?: () => void }) => {
+  return (<div class={styles.resultshadow}>
+    <div class={styles.result}>
+      <h2>終了!</h2>
+      <div>{prop.reason}</div>
+      <div>記録: {prop.history.length}単語</div>
+      <button onClick={prop.onRetry}>もう一回あそぶ</button>
+    </div>
+  </div >);
+};
